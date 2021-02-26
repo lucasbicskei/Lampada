d0c0cbc3a45ffe1d8af85df4e8a747133a923ba6
lucasbicskei <61919089+lucasbicskei@users.noreply.github.com>
2021-02-26 12:17:40 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+      // Lâmpada: Acender e Apagar
+
+      // github: @lucasbicskei
+
+function acender() {
+    document.getElementById("lampada").src = "../EXPERIENCIAS/imagens/acesa.jpg";
+}
+
+function apagar() {
+    document.getElementById("lampada").src = "../EXPERIENCIAS/imagens/apagada.jpg";
+}
